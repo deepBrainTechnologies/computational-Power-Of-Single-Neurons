c12b5ce617b96d83c05f4e3c1146915cd1169001
Isabel Delgado-Ruz(Delgado-Falker) <technologiesdeepbrain@gmail.com>
2025-07-21 22:24:00 -0400

    Update script_make_Xn11_input_signals_Paper2_Model2.m

--- Matlab/algorithms/ionChannelsWork/script_make_Xn11_input_signals_Paper2_Model2.m
+++ Matlab/algorithms/ionChannelsWork/script_make_Xn11_input_signals_Paper2_Model2.m
@@ -14,8 +14,16 @@
 %generate input signals  for Model 2 
 %                       Model 2 Case 1:(3 lines of fixed delays)
 %                       Model 2 Case 2:(3 lines of delays with PDF)
-OUTFOLDER = 'd:/DEEPBRAINTECHNOLOGIES/PAPERS/Model2_3DelayLines_ionChans/';
+maxJittersFactors_List = [0.25 0.3 0.35 0.4];
+RT_times_List = [0.8 1 1.2 1.4 1.6 2 2.2];
+
+OUTFOLDER = 'd:/SIMULATIONS_OUTPUT/PAPERS/Model2_3DelayLines_ionChans/';
 mkdir(OUTFOLDER);
+
+for jittIX= 1:1:length(maxJittersFactors_List)
+jittFact = maxJittersFactors_List(jittIX);
+for rttimeIX = 1:1:length(RT_times_List)
+rttimeThis = RT_times_List(rttimeIX);
 
 StepsSim  = 2000;
 nTrials   = 10;
@@ -26,11 +34,11 @@ dtSim     = 0.05; %us
 signals = zeros(nTrials,StepsSim);
 
 %----  define base times of Regular Delta times  ----
-RT_period   = 1;               %us  time between pulses, regularly spaced
+RT_period   = rttimeThis;               %us  time between pulses, regularly spaced
 RT_steps    = RT_period/dtSim; %sim steps between pulses
 
 %---  define (deltas) times  ---
-nPulses       = floor(StepsSim/RT_steps)-1;
+nPulses       = floor(StepsSim/RT_steps)-2;
 pulsesTimes    = RT_steps:RT_steps:(nPulses*RT_steps);
 pulsesTimes    = repmat(pulsesTimes,nTrials,1);
 nPulses       = size(pulsesTimes,2);                   %nPulses per trial
@@ -38,7 +46,7 @@ nPulsesTotal  = nPulses*nTrials;
 
 %---  define Jitter Limits  ---
 minJitter = 1;
-maxJitter = floor(RT_steps*0.3);
+maxJitter = floor(RT_steps*jittFact);
 
 %-- generate Jitter for each nPulses ... jitter PDF: Uniform Distribution
 
@@ -50,21 +58,27 @@ jitterValues = round(jitterValues);
 
 pulsesJitteredTimes = pulsesTimes(:) + jitterValues;
 pulsesJitteredTimes = reshape(pulsesJitteredTimes,nTrials,nPulses);
-signalJitter = signals;
+X11_signalJitter_binary = signals;
 for i=1:1:size(signals,1)
-  signalJitter(i,pulsesJitteredTimes(i,:))=1;
+  X11_signalJitter_binary(i,cast(pulsesJitteredTimes(i,:),'uint32'))=1;
 end
 
 PDFtype = 'uniform';
 DESTDIR = [OUTFOLDER '/' SUBFOLDER ];
 mkdir(DESTDIR);
-DESTDIR = sprintf('%s/PARAMETERS_RT_period_%dus__maxJitter_%dsimsteps/',...
+DESTDIR = sprintf('%s/PARAMETERS_RT_period_%1.1fus__maxJitter_%dsimsteps/',...
                   DESTDIR,RT_period,maxJitter);
+                
 mkdir(DESTDIR);
 save([DESTDIR 'X11_RSsignal_jitter_of_PDF.mat' ],...
-    'jitterValues','pulsesJitteredTimes','signalJitter',...
+    'jitterValues','pulsesJitteredTimes','X11_signalJitter_binary',...
     'StepsSim', 'nTrials', 'dtSim','pulsesTimes','nPulses','RT_period',...
     'maxJitter','minJitter','PDFtype');
+  
+%for simulink.... to plot on our prototyping scenario  
+simulink_X11_timeseries = [dtSim:dtSim:dtSim*size(X11_signalJitter_binary,2); X11_signalJitter_binary(1,:)];  
+save(  [DESTDIR 'X11_simulink_signal.mat'],'simulink_X11_timeseries');
+
 %--------------------------------------------------------------------------
 SUBFOLDER = '/EXPONENTIAL/';                                    
 jitterValues = classRandomNumbersGenerator.exponential(false,...
@@ -73,19 +87,19 @@ jitterValues = round(jitterValues+1);
 
 pulsesJitteredTimes = pulsesTimes(:) + jitterValues;
 pulsesJitteredTimes = reshape(pulsesJitteredTimes,nTrials,nPulses);
-signalJitter = signals;
-for i=1:1:size(signals,1)
-  signalJitter(i,pulsesJitteredTimes(i,:))=1;
+X11_signalJitter_binary = signals;
+for i=1:1:nTrials
+  X11_signalJitter_binary(i,cast(pulsesJitteredTimes(i,:),'uint32'))=1;
 end
 
 PDFtype = 'exponential';
 DESTDIR = [OUTFOLDER '/' SUBFOLDER ];
 mkdir(DESTDIR);
-DESTDIR = sprintf('%s/PARAMETERS_RT_period_%dus__maxJitter_%dsimsteps/',...
+DESTDIR = sprintf('%s/PARAMETERS_RT_period_%1.1fus__maxJitter_%dsimsteps/',...
                   DESTDIR,RT_period,maxJitter);
 mkdir(DESTDIR);
 save([DESTDIR 'X11_RSsignal_jitter_of_PDF.mat' ],...
-    'jitterValues','pulsesJitteredTimes','signalJitter',...
+    'jitterValues','pulsesJitteredTimes','X11_signalJitter_binary',...
     'StepsSim', 'nTrials', 'dtSim','pulsesTimes','nPulses','RT_period',...
     'maxJitter','minJitter','PDFtype');
 
@@ -99,22 +113,23 @@ jitterValues = round(jitterValues);
 
 pulsesJitteredTimes = pulsesTimes(:) + jitterValues;
 pulsesJitteredTimes = reshape(pulsesJitteredTimes,nTrials,nPulses);
-signalJitter = signals;
-for i=1:1:size(signals,1)
-  signalJitter(i,pulsesJitteredTimes(i,:))=1;
+X11_signalJitter_binary = signals;
+for i=1:1:nTrials
+  X11_signalJitter_binary(i,cast(pulsesJitteredTimes(i,:),'uint32'))=1;
 end
 
 PDFtype = 'gaussian';
 DESTDIR = [OUTFOLDER '/' SUBFOLDER ];
 mkdir(DESTDIR);
-DESTDIR = sprintf('%s/PARAMETERS_RT_period_%dus__maxJitter_%dsimsteps/',...
+DESTDIR = sprintf('%s/PARAMETERS_RT_period_%1.1fus__maxJitter_%dsimsteps/',...
                   DESTDIR,RT_period,maxJitter);
 mkdir(DESTDIR);
 save([DESTDIR 'X11_RSsignal_jitter_of_PDF.mat' ],...
-    'jitterValues','pulsesJitteredTimes','signalJitter',...
+    'jitterValues','pulsesJitteredTimes','X11_signalJitter_binary',...
     'StepsSim', 'nTrials', 'dtSim','pulsesTimes','nPulses','RT_period',...
     'maxJitter','minJitter','PDFtype');
 
 
-
+end
+end
 
