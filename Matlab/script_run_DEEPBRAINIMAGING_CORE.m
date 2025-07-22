6d3a4cd254668ba77ce076ecd412df299124e6e9
Isabel Delgado-Ruz(Delgado-Falker) <technologiesdeepbrain@gmail.com>
2025-07-21 22:29:36 -0400

    Create script_run_DEEPBRAINIMAGING_CORE.m

---
+++ Matlab/script_run_DEEPBRAINIMAGING_CORE.m
@@ -0,0 +1,12 @@
+%
+%  script_run_DEEPBRAINIMAGING_CORE 
+%
+%       defining global configuration variables
+%
+%       settingsCORE.RUN =1/0; to stop application
+%_____________________________________________________
+
+  settingsCORE.baseSettingsDIR = 'full path to settings files'
+  settingsCORE.Nthreads = 7;   %number of threads in parallel
+  
+  settingsCORE.RUN=1;           %0 to stop the application
