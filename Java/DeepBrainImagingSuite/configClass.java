8683a8986020f7a2b5859f1c05cbec50d8e0105d
Isabel Delgado-Ruz(Delgado-Falker) <technologiesdeepbrain@gmail.com>
2025-09-27 22:01:22 -0300

    Create configClass.java

---
+++ Java/DeepBrainImagingSuite/configClass.java
@@ -0,0 +1,13 @@
+/*
+*    ---------- public class configClass ------------
+*    DeepBrainTechnologies fundamentals and foundations:
+*        --configuration rules and determines architecture
+*        --1 class must have 1 constructor driven by config received.
+*        --we must return void almost always! (no try -> catches!)
+*/
+public class configClass{
+  public int nDevices
+  public confDevClass devices;   //configuration of devices to monitor (files, usbs, IoT, tcp, etc)
+  public confWinsClass windows;  //configuration of windows to display
+
+}
