bb0072eb55396f1094b53730241074040d0d2c92
Isabel Delgado-Ruz(Delgado-Falker) <technologiesdeepbrain@gmail.com>
2025-09-27 22:15:38 -0300

    Update and rename confDevClass.java to configDevClass.java

--- Java/DeepBrainImagingSuite/configDevClass.java
+++ Java/DeepBrainImagingSuite/configDevClass.java
@@ -1,11 +1,11 @@
 /*
-*    ---------- public class confDevClass (structure) ------------
+*    ---------- public class configDevClass (structure) ------------
 *    DeepBrainTechnologies fundamentals and foundations:
 *        --configuration rules and determines architecture
 *        --1 class must have 1 constructor driven by config received.
 *        --we must return void almost always! (no try -> catches!)
 */
-public class confDevClass{
+public class configDevClass{
   public int nDev;
   public int[] identifier; //identifier of each device example: 0002 0005 0045 0046 (range indicates type)
 }
