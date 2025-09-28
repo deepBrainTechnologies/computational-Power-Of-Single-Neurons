991a8513c4db295c75db69f7b192195390427e60
Isabel Delgado-Ruz(Delgado-Falker) <technologiesdeepbrain@gmail.com>
2025-09-27 21:22:25 -0300

    Create monitoringClass.java

---
+++ Java/DeepBrainImagingSuite/monitoringClass.java
@@ -0,0 +1,44 @@
+/*
+/    ---------- public class monitoringClass ------------
+/    DeepBrainTechnologies fundamentals and foundations:
+/        --configuration rules and determines architecture
+/        --1 class must have 1 constructor driven by config received.
+/        --we must return void almost always! (no try -> catches!)
+*/
+public class monitoringClass{
+  //properties ------------------------------
+  int nDev;
+  configClass config;    //stores configuration of entire suite
+  devicesClass devices;  //handles devices (usbs, tcp, IoT, files, etc)
+
+  //public methods ---------------------------------
+  public void monitoringClass(classConfig config){
+    this.config = config;  
+    this.addListOfDevices();
+  }
+
+  //startMonitoring
+  public void start(){
+    this.startMonitoringDevices();
+  }
+
+  //endMonitoring
+  public void end(){
+  }
+
+  //private methods ---------------------------------
+  private void addListOfDevices(void){
+    this.nDev = this.config.devices.nDevices;
+    for (int devIX=1; devIX<this.nDev; devIX++){
+        this.devices.addDevice(this.config.devices.identifier[devIX]);
+    } 
+  }
+
+  private void startMonitoringDevices(void){
+    for (int devIX=1; devIX<this.nDev; devIX++){
+        this.devices.startDevice(devIX);
+    } 
+  }
+  
+}
+  
