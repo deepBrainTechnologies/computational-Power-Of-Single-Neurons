a14b6be0dde85e54fae74c0069ed6a948c1d4caf
Isabel Delgado-Ruz(Delgado-Falker) <technologiesdeepbrain@gmail.com>
2025-09-28 15:17:27 -0300

    Update configDevClass.java

--- Java/DeepBrainImagingSuite/configDevClass.java
+++ Java/DeepBrainImagingSuite/configDevClass.java
@@ -3,9 +3,16 @@
 *    DeepBrainTechnologies fundamentals and foundations:
 *        --configuration rules and determines architecture
 *        --1 class must have 1 constructor driven by config received.
-*        --we must return void almost always! (no try -> catches!)
+*        --we must return void almost always! (never use try -> catches!)
 */
 public class configDevClass{
+  //properties ----------------------------------
   public int nDev;
   public int[] identifier; //identifier of each device example: 0002 0005 0045 0046 (range indicates type)
+
+  //methods -------------------------------------
+  public configDevClass(int nDev){
+    this.nDev = nDev;
+    this.identifier = new int[nDev];
+  }
 }
