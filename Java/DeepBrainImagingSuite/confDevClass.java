4fd6e2e0b1516f8256949f289d450487930bed65
Isabel Delgado-Ruz(Delgado-Falker) <technologiesdeepbrain@gmail.com>
2025-09-27 22:13:06 -0300

    Update confDevClass.java

--- Java/DeepBrainImagingSuite/confDevClass.java
+++ Java/DeepBrainImagingSuite/confDevClass.java
@@ -1,3 +1,10 @@
+/*
+*    ---------- public class confDevClass (structure) ------------
+*    DeepBrainTechnologies fundamentals and foundations:
+*        --configuration rules and determines architecture
+*        --1 class must have 1 constructor driven by config received.
+*        --we must return void almost always! (no try -> catches!)
+*/
 public class confDevClass{
   public int nDev;
   public int[] identifier; //identifier of each device example: 0002 0005 0045 0046 (range indicates type)
